69cf72edd25863195715d077565844dcdd41e0e3
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2020-01-15 17:50:53 -0600

    Included the validation step in the processing batch job

--- process.sh
+++ process.sh
@@ -1,14 +1,18 @@
+width=2000
 for file in `ls -1 *.tiff`; do
     dataset=`basename $file .tiff`
     echo Processing $dataset
-    if [ ! -f ${dataset}_smaller.png ]; then
-	echo Resizing to a width of 1000 pixels
-	convert -resize 1000x -transparent black $file ${dataset}_smaller.png
+    if [ ! -f ${dataset}_validation.png ]; then    
+	python3 validate.py $file # check the annotation locations by class
+    fi
+    if [ ! -f ${dataset}_smaller.png ]; then # resize for efficiency
+	echo Resizing to a width of $width pixels
+	convert -resize ${width}x -transparent black $file ${dataset}_smaller.png 
 	
     fi
     if [ ! -f ${dataset}_smaller_enhanced.png ]; then    
-	python3 enhance.py ${dataset}_smaller.png
+	python3 enhance.py ${dataset}_smaller.png # enhance color spectrum
     fi
-    python3 histogram.py ${dataset}_smaller_enhanced.png
+    python3 histogram.py ${dataset}_smaller_enhanced.png # analyze isolated class samples
 done
 
